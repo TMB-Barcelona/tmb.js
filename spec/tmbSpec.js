1fb83afc7160b75ad14da6d6433983790e713907
Micho Garcia <michogar@gmail.com>
2016-05-02 12:06:57 +0200

    Added rows as options to get more records from api Added spec

--- spec/tmbSpec.js
+++ spec/tmbSpec.js
@@ -42,6 +42,48 @@ describe("tmb.js spec:", function() {
             spyOn(http, 'get').and.callFake(function () {
                 return {
                     then: function (callback) {
+                        var response = readJSON('spec/fixtures/search.catalunya.20.json');
+                        return callback(response);
+                    }
+                }
+            });
+
+            function handleSuccess(response) {
+                result = response;
+                done()
+            }
+
+            function handleError(error) {
+                result = false;
+                done()
+            }
+
+            api.search('catalunya').then(handleSuccess, handleError);
+        });
+
+        it("as default should search for a term and return 20 records", function() {
+            expect(http.get).toHaveBeenCalled();
+            expect(http.get).toHaveBeenCalledWith('search', { params : { q : 'catalunya', rows: 20 } } );
+            expect(result.response.numFound).toBe(54);
+            expect(result.response.start).toBe(0);
+            expect(result.response.docs.length).toBe(20);
+        })
+    });
+    
+    describe("API search call", function() {
+        var result, http;
+
+        beforeEach(function(done) {
+            
+            var options = {};
+            options.rows = 10;
+
+            api = tmb(keys.app_id, keys.app_key, options);
+            
+            http = api.http;
+            spyOn(http, 'get').and.callFake(function () {
+                return {
+                    then: function (callback) {
                         var response = readJSON('spec/fixtures/search.catalunya.json');
                         return callback(response);
                     }
@@ -61,9 +103,9 @@ describe("tmb.js spec:", function() {
             api.search('catalunya').then(handleSuccess, handleError);
         });
 
-        it("should search for a term and return something", function() {
+        it("should search for a term and return 10 records passing rows parameter in options", function() {
             expect(http.get).toHaveBeenCalled();
-            expect(http.get).toHaveBeenCalledWith('search', { params : { q : 'catalunya' } } );
+            expect(http.get).toHaveBeenCalledWith('search', { params : { q : 'catalunya', rows: 10 } } );
             expect(result.response.numFound).toBe(54);
             expect(result.response.start).toBe(0);
             expect(result.response.docs.length).toBe(10);
--- src/tmb.js
+++ src/tmb.js
@@ -1,8 +1,21 @@
 var axios = require("axios");
-
-var api = function(app_id, app_key, version) {
+/**
+ * @classdesc
+ * Creates an API instance to use
+ * Get app_id and app_key from
+ *      https://developer.tmb.cat/
+ *      
+ *
+ * @param app_id
+ * @param app_key
+ * @param {object} options:
+ *      rows: indicates the number of records wo be returned (default 20)
+ *
+ * @api experimental
+ */
+var api = function(app_id, app_key, options) {
     var http = axios.create({
-        baseURL: "https://api.tmb.cat/v" + encodeURIComponent((version || 1).toString()) + "/",
+        baseURL: "https://api.tmb.cat/v" + encodeURIComponent((options && options.version || 1).toString()) + "/",
         params: {
             app_id: app_id,
             app_key: app_key
@@ -13,10 +26,20 @@ var api = function(app_id, app_key, version) {
         return response.data
     });
 
+    /**
+     * Search function. Receives a term to search and returns the promise will be resolved with the response.
+     * Use then(handleResponse, handleError) to get the response
+     *
+     * @param query term to search
+     * @returns {axios.Promise}
+     */
     function search(query) {
+        var rows = 20;
         return http.get("search", {
             params: {
-                q: query /*, fl: "*" */
+                q: query,
+                rows: (options && options.rows) ? options.rows : rows
+                /*, fl: "*" */
             }
         });
     }
