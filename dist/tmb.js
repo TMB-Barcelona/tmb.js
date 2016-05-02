98e6e886bbd8d4edc83718c435c64b23fa48c38a
Micho Garcia <michogar@gmail.com>
2016-05-02 13:20:05 +0200

    Added new dist after changes

--- dist/tmb.js
+++ dist/tmb.js
@@ -55,10 +55,23 @@ return /******/ (function(modules) { // webpackBootstrap
 /***/ function(module, exports, __webpack_require__) {
 
 	var axios = __webpack_require__(1);
-	
-	var api = function(app_id, app_key, version) {
+	/**
+	 * @classdesc
+	 * Creates an API instance to use
+	 * Get app_id and app_key from
+	 *      https://developer.tmb.cat/
+	 *      
+	 *
+	 * @param app_id
+	 * @param app_key
+	 * @param {object} options:
+	 *      rows: indicates the number of records wo be returned (default 20)
+	 *
+	 * @api experimental
+	 */
+	var api = function(app_id, app_key, options) {
 	    var http = axios.create({
-	        baseURL: "https://api.tmb.cat/v" + encodeURIComponent((version || 1).toString()) + "/",
+	        baseURL: "https://api.tmb.cat/v" + encodeURIComponent((options && options.version || 1).toString()) + "/",
 	        params: {
 	            app_id: app_id,
 	            app_key: app_key
@@ -69,10 +82,20 @@ return /******/ (function(modules) { // webpackBootstrap
 	        return response.data
 	    });
 	
+	    /**
+	     * Search function. Receives a term to search and returns the promise will be resolved with the response.
+	     * Use then(handleResponse, handleError) to get the response
+	     *
+	     * @param query term to search
+	     * @returns {axios.Promise}
+	     */
 	    function search(query) {
+	        var rows = 20;
 	        return http.get("search", {
 	            params: {
-	                q: query /*, fl: "*" */
+	                q: query,
+	                rows: (options && options.rows) ? options.rows : rows
+	                /*, fl: "*" */
 	            }
 	        });
 	    }
