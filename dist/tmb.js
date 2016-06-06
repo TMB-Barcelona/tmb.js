4b577f37be13e22668453374a6fe5c918b77033f
Micho Garcia <michogar@gmail.com>
2016-06-06 11:29:49 +0200

    Build tmb.js

--- dist/tmb.js
+++ dist/tmb.js
@@ -87,7 +87,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	        return response.data
 	    });
 	    
-	    var search = Search(http, options);
+	    var search = Search(http);
 	
 	    return {
 	        helloWorld: "Hello World! Your API keys are " + JSON.stringify(http.defaults.params),
@@ -1296,15 +1296,24 @@ return /******/ (function(modules) { // webpackBootstrap
 	 * 
 	 * 
 	 * @param http
-	 * @param options
 	 * @returns {{search: search}}
 	 */
 	
-	
 	'use strict';
 	
-	var Search = function(http, options) {
-	    
+	var Search = function(http) {
+	    var ENTITATS = {
+	        LINIES: 'Línies',
+	        INTERCANVIADORS: 'Intercanviadors',
+	        PARADES: 'Parades',
+	        ESTACIONS: 'Estacions',
+	        ACCESSOS: 'Accessos'
+	    };
+	
+	    var config = {
+	        resultsPerPage: 20
+	    };
+	
 	    /**
 	     * Search function. Receives a term to search and returns the promise will be resolved with the response.
 	     * Use then(handleResponse, handleError) to get the response
@@ -1312,23 +1321,36 @@ return /******/ (function(modules) { // webpackBootstrap
 	     * @param query term to search
 	     * @returns {axios.Promise}
 	     */
-	    function search(query) {
-	        var rows = 20;
+	    function query(text, options) {
+	        options = options || {};
+	
+	        var params = {
+	            q: text,
+	            rows: options.hasOwnProperty('resultsPerPage') ? options.resultsPerPage : config.resultsPerPage
+	        };
+	
+	        if (options.hasOwnProperty('entitats')) {
+	            params.entitats = [].concat(options.entitats).join();
+	        }
+	
+	        if (options.hasOwnProperty('detail') && options.detail == true) {
+	            params.fl = "*";
+	        }
+	
 	        return http.get("search", {
-	            params: {
-	                q: query,
-	                rows: (options && options.search && options.search.rows) ? options.search.rows : rows
-	                /*, fl: "*" */
-	            }
+	            params: params
 	        });
 	    }
 	
 	    return {
-	        search: search
-	    }
+	        ENTITATS: ENTITATS,
+	        config: config,
+	        query: query
+	    };
 	};
 	
 	module.exports = Search;
+
 
 /***/ }
 /******/ ])
