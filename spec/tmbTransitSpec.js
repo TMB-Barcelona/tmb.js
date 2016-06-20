97bb766dd40401abb85864875710a8fc1207cd03
Oscar Fonts <oscar.fonts@gmail.com>
2016-06-20 17:03:55 +0200

    Added parades i estacions. Closes #26

--- spec/tmbTransitSpec.js
+++ spec/tmbTransitSpec.js
@@ -43,4 +43,83 @@ describe("tmb.transit.js spec:", function() {
             }
         });
     });
+
+    describe("Get parades", function() {
+        var api;
+
+        beforeEach(function() {
+            api = tmb(keys.app_id, keys.app_key);
+        });
+
+        it("should get all parades by default", function(done) {
+            spyOn(api.http, 'get').and.callFake(function() {
+                return Promise.resolve(readJSON('spec/fixtures/transit.parades.json'));
+            });
+
+            api.transit.parades().then(handleSuccess, fail);
+
+            function handleSuccess(response) {
+                expect(api.http.get).toHaveBeenCalledWith('transit/parades/');
+                expect(response.totalFeatures).toBe(2722);
+                done();
+            }
+        });
+
+        it("should get parades with a particular code", function(done) {
+            spyOn(api.http, 'get').and.callFake(function() {
+                return Promise.resolve(readJSON('spec/fixtures/transit.parades.1244.json'));
+            });
+
+            api.transit.parades(1244).then(handleSuccess, fail);
+
+            function handleSuccess(response) {
+                expect(api.http.get).toHaveBeenCalledWith('transit/parades/1244');
+                expect(response.totalFeatures).toBe(1);
+                response.features.forEach(function(feature) {
+                    expect(feature.properties.CODI_PARADA).toBe(1244);
+                });
+                done();
+            }
+        });
+    });
+
+    describe("Get estacions", function() {
+        var api;
+
+        beforeEach(function() {
+            api = tmb(keys.app_id, keys.app_key);
+        });
+
+        it("should get all estacions by default", function(done) {
+            spyOn(api.http, 'get').and.callFake(function() {
+                return Promise.resolve(readJSON('spec/fixtures/transit.estacions.json'));
+            });
+
+            api.transit.estacions().then(handleSuccess, fail);
+
+            function handleSuccess(response) {
+                expect(api.http.get).toHaveBeenCalledWith('transit/estacions/');
+                expect(response.totalFeatures).toBe(130);
+                done();
+            }
+        });
+
+        it("should get estacions with a particular code", function(done) {
+            spyOn(api.http, 'get').and.callFake(function() {
+                return Promise.resolve(readJSON('spec/fixtures/transit.estacions.6660126.json'));
+            });
+
+            api.transit.estacions(6660126).then(handleSuccess, fail);
+
+            function handleSuccess(response) {
+                expect(api.http.get).toHaveBeenCalledWith('transit/estacions/6660126');
+                expect(response.totalFeatures).toBe(1);
+                response.features.forEach(function(feature) {
+                    expect(feature.properties.CODI_GRUP_ESTACIO).toBe(6660126);
+                });
+                done();
+            }
+        });
+    });
+
 });
--- src/transit/tmb.transit.js
+++ src/transit/tmb.transit.js
@@ -8,18 +8,16 @@
 
 var Transit = function(http) {
 
-    /**
-     * Get linies from all transport types (bus, subway and others) from Transit API.
-     *
-     * @param {Number} [codi] - The line code
-     * @returns {Promise} - A promise to manage response from server
-     */
-    function linies(codi) {
-        return http.get("transit/linies/" + (codi || ''));
-    }
-
     return {
-        linies: linies
+        linies: function(codi) {
+            return http.get("transit/linies/" + (codi || ''));
+        },
+        parades: function(codi) {
+            return http.get("transit/parades/" + (codi || ''));
+        },
+        estacions: function(codi) {
+            return http.get("transit/estacions/" + (codi || ''));
+        }
     };
 };
 
