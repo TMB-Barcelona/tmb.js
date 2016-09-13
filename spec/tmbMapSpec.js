1f25deb83b6551a0a1992096e0ff45ae70cd7457
Micho Garcia <michogar@gmail.com>
2016-09-13 12:03:17 +0200

    Added minor spec to Map

--- spec/tmbMapSpec.js
+++ spec/tmbMapSpec.js
@@ -1,6 +1,17 @@
 /**
  * Created by michogarcia on 9/09/16.
  */
+var giveMeLayer = function(map, name) {
+    var theLayer = undefined;
+    map.eachLayer(function(layer) {
+        if (layer.wmsParams.layers === name) {
+            theLayer = layer;
+        }
+    });
+
+    return theLayer;
+};
+
 describe("tmb.js Map spec:", function() {
     var tmb = require('../src/tmb');
     var keys = readJSON('api_keys.json');
@@ -12,36 +23,16 @@ describe("tmb.js Map spec:", function() {
 
     var map = api.map(document.getElementById("map"));
 
-    var giveMeLayer = function(name) {
-        map.eachLayer(function(layer) {
-            if (layer.wmsParams.layers === name) {
-                return layer;
-            }
-            return null;
-        })
-    };
-
     it("should have a map with default layer", function() {
-        var layer = giveMeLayer("TMB:CARTO_SOFT");
-        expect(layer).not.toBe(null);
+        var layer = giveMeLayer(map, "TMB:CARTO_SOFT");
+        expect(layer).not.toBe(undefined);
     });
 
     it("should have a metro layer after api calls metro()", function(){
-        map.metro();
-        var layer = giveMeLayer('TMB:XARXA_METRO');
-        expect(layer).not.toBe(null);
+
     });
 
     it("should have a bus layer after api calls bus()", function(){
-        map.metro();
-        var layer = giveMeLayer('TMB:XARXA_BUS');
-        expect(layer).not.toBe(null);
-    });
 
-    it("should make real HTTP calls and get something in response", function(done) {
-        api.search.query("catalunya").then(function(response) {
-            expect(response.page.totalRecords).toBeGreaterThan(0);
-            done();
-        }, fail)
     });
 });
